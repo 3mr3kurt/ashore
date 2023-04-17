8d1972cc2514f6596eb2e12f375f565af17d55e0
3mr3kurt <emrekurt5544@gmail.com>
2023-04-17 00:31:41 -0400

    v2

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
